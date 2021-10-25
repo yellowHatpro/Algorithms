a025f838013f1f7b421ce957ace8d46e7a0c5626
yellowHatpro <ashutoshaswal.btcse20@pec.edu.in>
2021-10-25 13:23:33 +0530

    Added Comments

--- BinaryExponential.cpp
+++ BinaryExponential.cpp
@@ -1,14 +1,14 @@
 long long binarypower(long long a , long long b){
 	if (b == 0)
-	{
-		return 1;
+	{//base case 
+		return 1;	
 	}
 
 	long long result = binarypower(a,b/2);
 	if (b%2) return result * result *a;
 	else return result*result;
 }
-
+//iterative
 long long binpower(long long a, long long b ){
 long long res = 1;
 while(b>0){
@@ -17,9 +17,17 @@ while(b>0){
 		res == res*a;
 	}
 		a= a *a;
-		b>>=1;
+		b>>=1; // b divides 
 	
 
 }	
 return res;
 }
+
+/*
+Time Complexity: O(logn)
+Applications:
+1. Effective computation of large exponents modulo a number
+2. Effective computation of  Fibonacci numbers 
+3. Applying permutation k times
+*/
