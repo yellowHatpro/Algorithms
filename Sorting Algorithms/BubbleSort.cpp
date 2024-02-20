81ee4b4e83543464115c5c8bc40b9d400691620d
yellowhatpro <yellowhatpro3119@gmail.com>
2024-02-20 22:21:08 +0530

    code restructure

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -8,26 +8,22 @@ Worst Case and Average Case Time Complexity: O(n2)
 Worst Case occurs when Array is reverse sorted.
 Best Case Time Complexity: O(n) : Array already sorted
 Auxiliary Space : O(1)
-Boundary Cases : Bubble sort takes min time (Order(n)) when elements are already sorted.
-In Place : Yes
-Stable : Yes
-Uses : Simple Algorithm , 
-	   Used to detect a very small error(like swap of just two elements) in almost sorted array, and fix it with just linear complexity (2n).
+Boundary Cases : Bubble sort takes min time (Order(n)) when elements are already
+sorted. In Place : Yes Stable : Yes Uses : Simple Algorithm , Used to detect a
+very small error(like swap of just two elements) in almost sorted array, and fix
+it with just linear complexity (2n).
 
 */
 
-int arr[10] = {9,8,7,6,5,4,3,2,1,0};
-int size = 10;
+int arr[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+int n = 10;
 
-void bubbleSort(){
-	for (int i = 0; i < size; ++i)
-	{
-		for (int j = 0; j < size-i-1; ++j)
-		{
-			if (arr[j]>arr[j+1])
-			{
-				swap(arr[j],arr[j+1]);
-			}
-		}
-	}
+void bubbleSort() {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n - i - 1; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
+      }
+    }
+  }
 }
--- Sorting Algorithms/InsertionSort.cpp
+++ Sorting Algorithms/InsertionSort.cpp
@@ -4,39 +4,39 @@ using namespace std;
 /*INSERTION SORT FACTS:
 Time Complexity : 0(n2)
 Auxiliary Space : O(1)
-Boundary Cases 	: Takes max time to sort if elements are sorted in Reverse order. 
-				  Takes min time (O(n)) when elements are already sorted.\
-Algorithm Paradigm : Incremental Approach
-Sorting in place : Yes
-Stable : Yes
-Online : Yes
-Uses : When number of elements are smaller in the array.
-	   When the array is almost sorted, only few elements are misplaced in completely big array.				  
+Boundary Cases 	: Takes max time to sort if elements are sorted in Reverse
+order.
+                                  Takes min time (O(n)) when elements are
+already sorted.\ Algorithm Paradigm : Incremental Approach Sorting in place :
+Yes Stable : Yes Online : Yes Uses : When number of elements are smaller in the
+array. When the array is almost sorted, only few elements are misplaced in
+completely big array.
 
 */
 
-int arr[10] = {9,8,7,6,5,4,3,2,1,0};
-int size = 10;
+int arr[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+int n = 10;
 
-void insertionSort(){
-	int key ;
-	for (int i = 1; i < size; ++i)
-	{
-		key = arr[i]; //We will check if any element is larger than this key,
-		int j=i-1; // and we will start checking in backward direction, starting from element just before the key element, that's why we start outer for loop with i=1
-		while(j>=0 && arr[j]>key){//if the current value is greater than the key,
-			arr[j+1] = arr[j]; // shift the value right 
-			j--;
-		}
-		arr[j+1] = key;
-	}
+void insertionSort() {
+  int key;
+  for (int i = 1; i < n; ++i) {
+    key = arr[i];  // We will check if any element is larger than this key,
+    int j = i - 1; // and we will start checking in backward direction, starting
+                   // from element just before the key element, that's why we
+                   // start outer for loop with i=1
+    while (j >= 0 &&
+           arr[j] > key) { // if the current value is greater than the key,
+      arr[j + 1] = arr[j]; // shift the value right
+      j--;
+    }
+    arr[j + 1] = key;
+  }
 }
 
-int main(){
-insertionSort();
-for (int i = 0; i < size; ++i)
-{
-	cout<<arr[i]<<" ";
-}
-cout<<endl;
+int main() {
+  insertionSort();
+  for (int i = 0; i < n; ++i) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 }
